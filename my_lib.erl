21341a21687f313167e10d469f80d8d7aa607b14
adilson.delanhese <adilson.delanhese@ekinops.com>
2023-04-05 10:13:57 -0300

    Added show devices function
    Change-Id: If6101e47a673cfab79808cf636fcfbe4b382a8d2

--- my_lib.erl
+++ my_lib.erl
@@ -52,9 +52,14 @@
 -export([check_file_exists/1]).
 -export([check_for_supported_devices/4]).
 -export([enable_disable_device/4]).
+-export([show_help/0]).
+-export([show_devices/0]).
 
 
 
+
+
+-define(MODULE_NAME, "updater_hw_devices").
 
 -define(INI_FILE, "arq.ini").
 -define(IMAGES_PATH, "/opt/ets/hw/images/").
@@ -1006,65 +1011,15 @@ check_for_supported_devices_search_for_device(Board, Device, CurrentBoard, Activ
 
 
 
-%    Arguments:
-%#   none
-%# Returns:
-%#   String with the path + file name
-%#
-%enable_disable_device() {
-%local device_to_change=$1
-%local new_state=$2
-%local board=${NULL}
-%local device=${NULL}
-%local alias=${NULL}
-%local index=0
-%local res=1
-%local num_devices=0
-%local device1=${NULL}
-%local alias1=${NULL}
-%local dependencies1=${NULL}
-%local device_dependent=${NULL}
+%--------------------------------------------------------------
 %
-%    board=$(extract_board ${device_to_change})
-%    device=$(extract_device ${device_to_change})
-%    alias=$(extract_alias ${device_to_change})
 %
-%    check_for_supported_devices ${board}_${device}
-%    res=$?
-%
-%    if [[ $res != ${SUCCESS} ]]; then
-%        exit_code=$(code_error $MODULE_MAIN $LINENO $DEVICE_NOT_SUPPORTED_ERROR)
-%        log_msg ${ERR} "${device_to_change}: $(error2str $exit_code)"
-%        return $exit_code
-%    fi
-%
-%    num_devices=$(read_cfg_file ${board} num_devices)
-%    dependencies1=$(read_field_from_cfg_anyway ${board} ${device} dependencies)
-%    device_dependent=$(extract_device ${dependencies1})
-%
-%    for (( d = 1; d <= $num_devices; d++ )); do
-%        device1=$(read_cfg_file ${board} device${d})
-%        alias1=$(read_cfg_file ${board} alias${d})
-%        dependencies1=$(read_cfg_file ${board} dependencies${d})
-%
-%        if [[ ${device1} == ${device} || ${device1} == ${device_dependent} ]]; then
-%            write_cfg_file ${board} enabled${d} 0
-%        fi
-%
-%        if [[ ${device1} == ${device} || ${device1} == ${device_dependent} ]] && [[ ${alias1} == ${alias} ]]; then
-%            write_cfg_file ${board} enabled${d} ${new_state}
-%        fi
-%    done
-%
-%    return $SUCCESS
-%}
-%
+%--------------------------------------------------------------
+-spec enable_disable_device(string, string, string, string) -> ok | error.
 enable_disable_device(BoardDeviceAlias, NewState, CurrentBoard, Active) ->
     Board = extract_board(BoardDeviceAlias),
     Device = extract_device (BoardDeviceAlias),
     Alias = extract_alias (BoardDeviceAlias),
-
-    io:format("~p ~p ~p ~n", [Board, Device, Alias]),
 
     case check_for_supported_devices(Board, Device, CurrentBoard, Active) of
         ok ->
@@ -1072,8 +1027,6 @@ enable_disable_device(BoardDeviceAlias, NewState, CurrentBoard, Active) ->
             {Result2, Dependencies1} = read_field_from_cfg_anyway(Board, Device, "dependencies"),
             Device_dependent = extract_device(Dependencies1),
             {Num_devices, _} = string:to_integer(Num_devicesStr),
-
-            io:format("Entry... ~p ~p ~p ~n", [Num_devicesStr, Dependencies1, Device_dependent]),
 
             if
                 (Result1 == ok) and (Result2 == ok) ->
@@ -1094,11 +1047,9 @@ disable_device(Board, Device, Device_dependent, Index, MaxDevices) ->
         (Index =< MaxDevices) ->
             {Result1, Device1} = ini_file(?INI_FILE, Board, unicode:characters_to_list(["device", integer_to_list(Index)])),
 
-
             if
                 (Result1 == ok) and
                 ((Device1 == Device) or (Device1 == Device_dependent)) ->
-                    io:format("Disabling... ~p ~p ~n", [Device1, Device_dependent]),
                     ini_file(?INI_FILE, Board, unicode:characters_to_list(["enabled", integer_to_list(Index)]), "0", wr),
                     disable_device(Board, Device, Device_dependent, Index + 1, MaxDevices);
 
@@ -1118,12 +1069,10 @@ enable_device(Board, Device, Device_dependent, Alias, NewState, Index, MaxDevice
             {Result1, Device1} = ini_file(?INI_FILE, Board, unicode:characters_to_list(["device", integer_to_list(Index)])),
             {Result2, Alias1} = ini_file(?INI_FILE, Board, unicode:characters_to_list(["alias", integer_to_list(Index)])),
 
-
             if
                 (Result1 == ok) and (Result2 == ok)  and
                 ((Device1 == Device) or (Device1 == Device_dependent)) and
                 (Alias == Alias1) ->
-                    io:format("Changing to: ~p => ~p ~p ~p ~n", [NewState, Device1, Alias1, Device_dependent]),
                     ini_file(?INI_FILE, Board, unicode:characters_to_list(["enabled", integer_to_list(Index)]), NewState, wr),
                     enable_device(Board, Device, Device_dependent, Alias, NewState, Index + 1, MaxDevices);
 
@@ -1134,3 +1083,205 @@ enable_device(Board, Device, Device_dependent, Alias, NewState, Index, MaxDevice
         true ->
             ok
     end.
+
+%--------------------------------------------------------------
+%
+% io:format(
+%--------------------------------------------------------------
+show_help() ->
+    io:format("~s: This tool provides the user interface to check and update the hw devices.~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("Tool usage:~n"),
+    io:format("     ~s [options...] [command <cmd_arg>]~n", [?MODULE_NAME]),
+    io:format("~n"),
+    io:format("Options list: ~n"),
+    io:format("    -a, --auto                                = automatically update all outdated devices~n"),
+    io:format("    -b, --backplane                           = force access to backplane (active card)~n"),
+    io:format("    -f, --file                                = show configuration file name~n"),
+    io:format("    -g, --background                          = run in background mode (RECOMMENDED!!!)~n"),
+    io:format("    -s, --spk                                 = check device version from spk image~n"),
+    io:format("    -v=<level>, --verbose=<level>             = set the log level to show messages (see note 1)~n"),
+    io:format("    -r, --fpga-reload                         = perform system reboot and force fpga reload after update~n"),
+    io:format("    -p, --power-cycle                         = perform power cycle after update~n"),
+    io:format("    -l, --system-reboot                       = perform system reboot after update~n"),
+    io:format("    -i=<file> or --input=<file>               = force an input file name to burn the device~n"),
+    io:format("    -o=<cardid> or --cardid=<cardid>          = force a card id (see note 2)~n"),
+    io:format("    -q=<chassisid> or --chassisid=<chassisid> = force a chassis id (see note 3)~n"),
+    io:format("    -t, --dtb=<dtb file>                      = specify a new dtb file~n"),
+    io:format("~n"),
+    io:format("Commands list: ~n"),
+    io:format("    -h, --help                                     = show this message help~n"),
+    io:format("    -c, --check                                    = check for all device versions~n"),
+    io:format("    -u=<device> or --update=<device>               = perform to device update (see note 4)~n"),
+    io:format("    -d=<device/alias> or --disable=<device/alias>  = disable device from list (see note 4)~n"),
+    io:format("    -e=<device/alias> or --enable=<device/alias>   = enable device from list (see note 4)~n"),
+    io:format("    -x=<device> or --examine=<device>  = examine for the current alias (see note 4)~n"),
+    io:format(" ~n"),
+    io:format(" Notes:~n"),
+    io:format("           1) verbose mode levels~n"),
+    io:format("              -v=0  no message to be shown~n"),
+    io:format("              -v=1  includes 'err', 'emerg', 'alert' and 'crit' messages~n"),
+    io:format("              -v=2  includes 'warning' and 'notice' messages~n"),
+    io:format("              -v=3  includes 'info' messages~n"),
+    io:format("              -v=4  includes 'debug' messages~n"),
+    io:format(" ~n"),
+    io:format("           2) card id values must be the card where the script is running. ~n"),
+    io:format("              -o=lc1~n"),
+    io:format("              -o=lc4~n"),
+    io:format("              -o=lc5~n"),
+    io:format("              -o=mngt~n"),
+    io:format("              -o=sc2000~n"),
+    io:format(" ~n"),
+    io:format("               Note: If no card id is specified then the script will try discovery it.~n"),
+    io:format(" ~n"),
+    io:format("           3) chassis id values must be the chassis where the script is running~n"),
+    io:format("              -q=etsc1~n"),
+    io:format("              -q=etsc2~n"),
+    io:format("              -q=etsc6~n"),
+    io:format(" ~n"),
+    io:format("              Note: If no chassis id is specified then the script will try discovery it.~n"),
+    io:format(" ~n"),
+
+%    show_devices
+
+    io:format(" ~n"),
+    io:format(" Examples:~n"),
+    io:format("       1) Checking for all devices versions (no verbose):~n"),
+    io:format("          # ~s -c~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("       2) Checking for all devices versions (verbose mode):~n"),
+    io:format("          # ~s -v=3 -c ~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("       3) Checking for all versions and update them automatically (verbose mode):~n"),
+    io:format("          # ~s -v=3 -a -c~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("       4) Updating a specified device (verbose mode):~n"),
+    io:format("          # ~s -v=3 -u=lc1_lpc55~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("       5) Disabling a specified device (verbose mode):~n"),
+    io:format("          # ~s -v=3 -d=lc1_fpgajic/otu2~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("       6) Enabling a specified device, automaticaly updating and~n"),
+    io:format("          rebooting the board after the update (verbose mode):~n"),
+    io:format("          # ~s -v=3 -r -e=lc1_fpgajic/otu2~n", [?MODULE_NAME]),
+    io:format(" ~n"),
+    io:format("       7) Running in background mode avoiding interruption during device update:~n"),
+    io:format("          # ~s -g -r -e=lc1_fpgajic/otu2~n", [?MODULE_NAME]),
+    io:format(" ~n").
+
+%# Arguments:
+%#   none
+%# Returns:
+%#   0 on success
+%#   1 on fail
+%show_devices() {
+%local num_boards=0
+%local board=${NULL}
+%local num_devices=0
+%local device=${NULL}
+%local alias=${NULL}
+%local run_at_active=0
+%local checkversion=0
+%local enabled=0
+%local base_board=0
+%local backplane_board=0
+%
+%    num_boards=$(read_cfg_file boards num_boards)
+%
+%    if [[ ${num_boards} == ${NULL} || ${num_boards} == 0 ]]; then
+%        return $SUCCESS
+%    fi
+%
+%    echo -e "           ${BMAGENTA}4) Supported devices for update:${RESET}"
+%
+%    for (( b = 1; b <= $num_boards; b++ )); do
+%        board=$(read_cfg_file boards board${b})
+%        num_devices=$(read_cfg_file ${board} num_devices)
+%
+%        for (( d = 1; d <= $num_devices; d++ )); do
+%            device=$(read_cfg_file ${board} device${d})
+%            run_at_active=$(read_cfg_file ${board} activecard${d})
+%            checkversion=$(read_cfg_file ${board} checkversion${d})
+%            enabled=$(read_cfg_file ${board} enabled${d})
+%            alias=$(read_cfg_file ${board} alias${d})
+%            base_board=0
+%            backplane_board=0
+%
+%            if [ ${board} == ${board_type} ]; then
+%                base_board=1
+%            fi
+%
+%            if [[ 1 == ${run_at_active}  &&  1 == ${active} ]]; then
+%                backplane_board=1
+%            fi
+%
+%            if [[ 1 == ${base_board} || 1 == ${backplane_board} ]]  &&  [[ 1 == ${enabled} ]]; then
+%                if [[ ${alias} == ${NULL} ]]; then
+%                    echo -e "                      ${BMAGENTA}${board}_${device}${RESET}"
+%                else
+%                    echo -e "                      ${BMAGENTA}${board}_${device} (${alias})${RESET}"
+%                fi
+%                
+%            fi
+%        done
+%    done
+%
+%    return $SUCCESS
+%}
+
+%-spec show_devices(string, string) -> {result, string}.
+%show_devices(CurrentBoard, Active) ->
+show_devices() ->
+    {Result1, NumBoardsStr} = ini_file(?INI_FILE, "boards", "num_boards"),
+
+    case Result1 of
+        ok ->
+            {NumBoards, _} = string:to_integer(NumBoardsStr),
+            show_devices_next_board(1, NumBoards);
+
+        _ ->
+            error
+    end.
+
+show_devices_next_board(Index, MaxBoards) ->
+
+    if
+        (Index =< MaxBoards) ->
+            {Result1, Board} = ini_file(?INI_FILE, "boards", unicode:characters_to_list(["board", integer_to_list(Index)])),
+
+            if
+                (Result1 == ok) ->
+                    io:format("~s~n", [Board]),
+
+                    {_, NumDevicesStr} = ini_file(?INI_FILE, Board, "num_devices"),
+                    {NumDevices, _} = string:to_integer(NumDevicesStr),
+                    show_devices_next_Device(Board, 1, NumDevices),
+                    show_devices_next_board(Index + 1, MaxBoards);
+
+                true ->
+                    error
+            end;
+
+        true ->
+            error
+    end.
+
+show_devices_next_Device(Board, Index, MaxDevices) ->
+
+    if
+        (Index =< MaxDevices) ->
+            {Result1, Device} = ini_file(?INI_FILE, Board, unicode:characters_to_list(["device", integer_to_list(Index)])),
+
+            if
+                (Result1 == ok) ->
+                    io:format("     ~s~n", [Device]),
+                    show_devices_next_Device(Board, Index + 1, MaxDevices);
+
+                true ->
+                    error
+            end;
+
+        true ->
+            error
+    end.
+
